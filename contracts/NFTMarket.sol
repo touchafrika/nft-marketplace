aec5f017b8db7ffe60f7f0cbb2d7d934a71d5fd6
Thabo Mayika <mayikathabo@gmail.com>
2022-01-13 18:52:13 +0000

    started transact process

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    function NFT(address _contractAddress) public{
+        
+    }
+}
