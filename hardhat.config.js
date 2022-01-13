0443164fc0bf6d457fca01005e396d5c64ba6c35
Thabo Mayika <mayikathabo@gmail.com>
2022-01-13 13:42:32 +0000

    configured app

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomiclabs/hardhat-waffle");
+const dotenv = required("dotenv");
+dotenv.config();
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [`${process.env.SECRET_KEY}`],
+    },
+  },
+  solidity: "0.8.4",
+};
